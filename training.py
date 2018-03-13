ced9448213cb1cad2f3c1bbc599ccba9399fd980
KUASWoodyLIN <jj7158761@gmail.com>
2018-03-13 20:19:12 +0800

    version v0.2 add training

--- training.py
+++ training.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPool2D, Dense, Flatten
+from keras.layers import Input, Conv2D, MaxPool2D, Dense, Flatten, Dropout
 
 ROOT_DIR = os.getcwd()
 IMAGES_DIR = os.path.join(ROOT_DIR, 'IMG')
@@ -29,7 +29,35 @@ for line in lines:
 x_train = np.array(images)
 y_train = np.array(measurements)
 
+x_input = Input(shape=(160, 320, 3), name='x_input')
 
+conv1 = Conv2D(32, (3,3), padding='same', activation='relu')(x_input)
+conv2 = Conv2D(32, (3,3), padding='same', activation='relu')(conv1)
+pool1 = MaxPool2D()(conv2)
 
+conv3 = Conv2D(64, (3,3), padding='same', activation='relu')(pool1)
+conv4 = Conv2D(64, (3,3), padding='same', activation='relu')(conv3)
+pool2 = MaxPool2D()(conv4)
 
-print()
+conv5 = Conv2D(128, (3,3), padding='same', activation='relu')(pool2)
+pool3 = MaxPool2D()(conv5)
+
+flat = Flatten()(pool3)
+
+hidden1 = Dense(256, activation='relu')(flat)
+drop1 = Dropout(0.5)(hidden1)
+
+hidden2 = Dense(128, activation='relu')(drop1)
+drop2 = Dropout(0.5)(hidden2)
+
+hidden3 = Dense(32, activation='relu')(drop2)
+drop3 = Dropout(0.5)(hidden3)
+
+output = Dense(1)(drop3)
+
+model = Model(x_input, output)
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(x_train, y_train, batch_size=64, epochs=10, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')

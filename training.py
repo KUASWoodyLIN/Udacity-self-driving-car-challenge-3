3c467bb93cda441f6a28dcbd9458eb16b32753df
KUASWoodyLIN <jj7158761@gmail.com>
2018-03-20 11:27:47 +0800

    version v2.1 add Lenet and callback

--- training.py
+++ training.py
@@ -1,17 +1,20 @@
 import os
 import csv
+from glob import glob
 
 import cv2
 import numpy as np
 
 from sklearn.model_selection import train_test_split
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPool2D, Dense, Flatten, Dropout
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint,TensorBoard
 
 # Path Setting
 ROOT_DIR = os.getcwd()
 IMAGES_DIR = os.path.join(ROOT_DIR, 'IMG')
 CSV_FILE = os.path.join(ROOT_DIR, 'driving_log.csv')
+LOGS_PATH = os.path.join(ROOT_DIR, 'logs')
 
 # Read csv file
 lines = []
@@ -57,44 +60,74 @@ def generator(x, y, batch_size):
 def main():
   x_input = Input(shape=(160, 320, 3), name='x_input')
 
-  conv1 = Conv2D(32, (3,3), padding='same', activation='relu')(x_input)
-  conv2 = Conv2D(32, (3,3), padding='same', activation='relu')(conv1)
-  pool1 = MaxPool2D()(conv2)
+  # LeNet
+  conv1 = Conv2D(6, (5, 5), padding='same', activation='relu')(x_input)
+  pool1 = MaxPooling2D()(conv1)
+  conv2 = Conv2D(6, (5, 5), padding='same', activation='relu')(pool1)
+  pool2 = MaxPooling2D()(conv2)
+  flat = Flatten()(pool2)
+  hedden1 = Dense(120)(flat)
+  hedden2 = Dense(84)(hedden1)
+  output = Dense(1)(hedden2)
 
-  conv3 = Conv2D(64, (3,3), padding='same', activation='relu')(pool1)
-  conv4 = Conv2D(64, (3,3), padding='same', activation='relu')(conv3)
-  pool2 = MaxPool2D()(conv4)
 
-  conv5 = Conv2D(128, (3,3), padding='same', activation='relu')(pool2)
-  pool3 = MaxPool2D()(conv5)
+  # VGG-16
+  # conv1 = Conv2D(32, (3,3), padding='same', activation='relu')(x_input)
+  # conv2 = Conv2D(32, (3,3), padding='same', activation='relu')(conv1)
+  # pool1 = MaxPool2D()(conv2)
+  #
+  # conv3 = Conv2D(64, (3,3), padding='same', activation='relu')(pool1)
+  # conv4 = Conv2D(64, (3,3), padding='same', activation='relu')(conv3)
+  # pool2 = MaxPool2D()(conv4)
+  #
+  # conv5 = Conv2D(128, (3,3), padding='same', activation='relu')(pool2)
+  # pool3 = MaxPool2D()(conv5)
+  #
+  # flat = Flatten()(pool3)
 
-  flat = Flatten()(pool3)
-
-  hidden1 = Dense(256)(flat)
-  drop1 = Dropout(0.5)(hidden1)
-
-  hidden2 = Dense(128)(drop1)
-  drop2 = Dropout(0.5)(hidden2)
-
-  hidden3 = Dense(32)(drop2)
-  drop3 = Dropout(0.5)(hidden3)
-
-  output = Dense(1)(drop3)
+  # hidden1 = Dense(256)(flat)
+  # drop1 = Dropout(0.5)(hidden1)
+  #
+  # hidden2 = Dense(128)(drop1)
+  # drop2 = Dropout(0.5)(hidden2)
+  #
+  # hidden3 = Dense(32)(drop2)
+  # drop3 = Dropout(0.5)(hidden3)
+  #
+  # output = Dense(1)(drop3)
 
   model = Model(x_input, output)
   model.compile(optimizer='adam', loss='mse')
   model.summary()
   print("")
 
-  # model.fit(x_train, y_train, batch_size=64, epochs=10, validation_split=0.2, shuffle=True, verbose=2)
+  early_stop = EarlyStopping(monitor='val_loss',
+                             min_delta=0.001,
+                             patience=3,   # 如果超過3次的循環在loss的收斂上沒有改善就停止
+                             mode='min',
+                             verbose=1)
+
+  checkpoint = ModelCheckpoint('model.h5',
+                               monitor='val_loss',
+                               verbose=1,
+                               save_best_only=True,
+                               mode='min',
+                               period=1)
+
+  log_file_name = 'Driving_Car_' + str(len(glob(LOGS_PATH + '/Driving_Car_*')) + 1)
+
+  tensorboard = TensorBoard(log_dir='./logs/' + log_file_name,
+                            histogram_freq=0,
+                            write_grads=True,
+                            write_images=False)
 
   # Add callback
   model.fit_generator(generator=generator(x_train, y_train, 64),
-                      steps_per_epoch=int(np.ceil(len(x_train)*2/64)), epochs=3,
+                      steps_per_epoch=int(np.ceil(len(x_train)*2/64)), epochs=10,
                       verbose=1,
                       validation_data=generator(x_valid, y_valid, 64),
-                      validation_steps=int(np.ceil(len(x_valid)*2/64)))
-  model.save('model.h5')
+                      validation_steps=int(np.ceil(len(x_valid)*2/64)),
+                      callbacks=[early_stop, checkpoint, tensorboard])
 
 
 if __name__ == '__main__':
